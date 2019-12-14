993a561e8a5687a81a1784cbb8543d4e5c464b46
ValentinZeller <v.zeller@ludus-academie.com>
2019-12-14 16:43:04 +0100

    Action Responses

---
+++ Assets/Scripts/ChangeRoomResponse.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (menuName = "TextAdventure/ActionResponses/ChangeRoom")]
+public class ChangeRoomResponse : ActionResponse
+{
+    public Room roomToChange;
+
+    public override bool DoActionResponse(GameController controller)
+    {
+        if (controller.roomNavigation.currentRoom.roomName == requiredString)
+        {
+            controller.roomNavigation.currentRoom = roomToChange;
+            controller.DisplayRoomText();
+            return true;
+        }
+        return false;
+    }
+}
+
