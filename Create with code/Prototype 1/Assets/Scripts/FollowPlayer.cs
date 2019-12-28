adcd0bd38a618c3187640d69b3d2bd07636af5b4
ValentinZeller <v.zeller@ludus-academie.com>
2019-12-28 16:41:24 +0100

    Lesson 1.4 - Step into the Driver's Seat

--- Create with code/Prototype 1/Assets/Scripts/FollowPlayer.cs
+++ Create with code/Prototype 1/Assets/Scripts/FollowPlayer.cs
@@ -7,8 +7,14 @@ public class FollowPlayer : MonoBehaviour
     public GameObject player;
     private Vector3 offset = new Vector3(0, 5, -7);
 
+    void Start()
+    {
+        
+    }
+
     void Update()
     {
+        //Offset the camera behind the player by adding to the player's position
         transform.position = player.transform.position + offset;
     }
 }
