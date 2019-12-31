f9e7c7e07d0429bd53b299e17b6e65bedcdde808
ValentinZeller <v.zeller@ludus-academie.com>
2019-12-31 14:28:04 +0100

    Lesson 3.2 - Make the World Whiz By

--- Create with code/Prototype 3/Assets/Scripts/MoveLeft.cs
+++ Create with code/Prototype 3/Assets/Scripts/MoveLeft.cs
@@ -4,16 +4,29 @@ using UnityEngine;
 
 public class MoveLeft : MonoBehaviour
 {
+    private PlayerController playerControllerScript;
     private float speed = 30;
+    private float leftBound = -15;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.left * Time.deltaTime * speed);
+        //Objects move to the left when the game is not over
+        if (playerControllerScript.gameOver == false)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+
+        //Destroy obstacles too far to the left
+        if (transform.position.x < leftBound && gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(gameObject);
+        }
     }
 }
