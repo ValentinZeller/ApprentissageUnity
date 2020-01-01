de2298b6c5de2468ddd0050de2d4b8f7c2d15720
ValentinZeller <v.zeller@ludus-academie.com>
2020-01-01 13:45:41 +0100

    Lesson 5.2 - Keeping Score

--- Create with code/Prototype 5/Assets/Scripts/Target.cs
+++ Create with code/Prototype 5/Assets/Scripts/Target.cs
@@ -11,13 +11,18 @@ public class Target : MonoBehaviour
     private float xRange = 4;
     private float ySpawnPos = -6;
 
+    private GameManager gameManager;
+    public int pointValue;
+    public ParticleSystem explosionParticle;
+
     // Start is called before the first frame update
     void Start()
     {
         targetRb = GetComponent<Rigidbody>();
         targetRb.AddForce(RandomForce(), ForceMode.Impulse);
         targetRb.AddTorque(RandomTorque(),RandomTorque(),RandomTorque(), ForceMode.Impulse);
-        transform.position = RandomSpawnPos();  
+        transform.position = RandomSpawnPos();
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
@@ -42,6 +47,8 @@ public class Target : MonoBehaviour
     private void OnMouseDown()
     {
         Destroy(gameObject);
+        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+        gameManager.UpdateScore(pointValue);
     }
     private void OnTriggerEnter(Collider other)
     {
