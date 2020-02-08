947e2448a4e86b58da01f954bfba02813b7e1d42
ValentinZeller <v.zeller@ludus-academie.com>
2020-02-08 18:24:26 +0100

    Unit Mechanics

--- Roguelike/Assets/Scripts/GameManager.cs
+++ Roguelike/Assets/Scripts/GameManager.cs
@@ -6,8 +6,13 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager instance = null;
     public BoardManager boardScript;
+    public int playerFoodPoints = 100;
+    [HideInInspector] public bool playersTurn = true;
+    public float turnDelay = 0.1f;
 
+    List<Enemy> enemies;
     int level = 3;
+    bool enemiesMoving;
 
     // Start is called before the first frame update
     void Awake()
@@ -21,18 +26,55 @@ public class GameManager : MonoBehaviour
         }
 
         DontDestroyOnLoad(gameObject);
+        enemies = new List<Enemy>();
         boardScript = GetComponent<BoardManager>();
         InitGame();
     }
 
     void InitGame()
     {
+        enemies.Clear();
         boardScript.SetupScene(level);
+    }
+
+    public void GameOver()
+    {
+        enabled = false;
+    }
+
+    IEnumerator MoveEnemies()
+    {
+        enemiesMoving = true;
+        yield return new WaitForSeconds(turnDelay);
+        if (enemies.Count == 0)
+        {
+            yield return new WaitForSeconds(turnDelay);
+        }
+
+        for (int i=0; i < enemies.Count;i++)
+        {
+            enemies[i].MoveEnemy();
+            yield return new WaitForSeconds(enemies[i].moveTime);
+        }
+
+        playersTurn = true;
+        enemiesMoving = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (playersTurn || enemiesMoving)
+        {
+            return;
+        } else
+        {
+            StartCoroutine(MoveEnemies());
+        }
+    }
+
+    public void AddEnemyToList(Enemy script)
+    {
+        enemies.Add(script);
     }
 }
